7fea80c3a0bcd676af851189b92a2abed229942c
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-22 01:36:26 +0300

    work

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(job = "go to the office") {
+    return `This Monday, I will ${job}.`;
+}
+
+function wrapAdjective(bold = "*") {
+    return function(word = "special") {
+        return `You are ${bold}${word}${bold}!`;
+    }
+}
